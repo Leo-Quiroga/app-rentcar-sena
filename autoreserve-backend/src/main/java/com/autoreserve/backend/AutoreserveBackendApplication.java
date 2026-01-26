c1d1a9cbb21c6b4c33262af27c1b3b8f99612943
Hernán Leonardo Quiroga Beltrán <hleonardoquirogab@hotmail.com>
2026-01-26 12:31:23 -0500

    organiza proyecto carpetas back, front y se crear proyecto back

---
+++ autoreserve-backend/src/main/java/com/autoreserve/backend/AutoreserveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autoreserve.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoreserveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoreserveBackendApplication.class, args);
+	}
+
+}
---
+++ autoreserve-backend/src/test/java/com/autoreserve/backend/AutoreserveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoreserve.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoreserveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
